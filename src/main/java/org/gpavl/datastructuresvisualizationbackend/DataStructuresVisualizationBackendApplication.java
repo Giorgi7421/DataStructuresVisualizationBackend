4d129b636ae4351d207da46164a9bb33635803cd
Xorxe7421 <pavliashviligiorgi7421@gmail.com>
2025-03-15 18:42:05 +0400

    initial commit

---
+++ src/main/java/org/gpavl/datastructuresvisualizationbackend/DataStructuresVisualizationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.gpavl.datastructuresvisualizationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresVisualizationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructuresVisualizationBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gpavl/datastructuresvisualizationbackend/DataStructuresVisualizationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gpavl.datastructuresvisualizationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresVisualizationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
