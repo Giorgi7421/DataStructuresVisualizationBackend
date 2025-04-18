436f9db7b09057b94223e65ded97f4af94f1e1ea
Xorxe7421 <pavliashviligiorgi7421@gmail.com>
2025-04-18 23:20:04 +0400

    Added 2 more priority queue implementations

--- src/main/java/org/gpavl/datastructuresvisualizationbackend/model/queue/SortedLinkedListPriorityQueue.java
+++ src/main/java/org/gpavl/datastructuresvisualizationbackend/model/queue/SortedLinkedListPriorityQueue.java
@@ -1,10 +1,14 @@
 package org.gpavl.datastructuresvisualizationbackend.model.queue;
 
 import org.gpavl.datastructuresvisualizationbackend.model.MemoryHistoryDto;
+import org.gpavl.datastructuresvisualizationbackend.model.Node;
 import org.gpavl.datastructuresvisualizationbackend.model.OperationHistoryDto;
 import org.gpavl.datastructuresvisualizationbackend.util.MemoryUtils;
 
 import java.util.Collections;
+import java.util.List;
+
+import static org.gpavl.datastructuresvisualizationbackend.util.MemoryUtils.*;
 
 public class SortedLinkedListPriorityQueue extends Queue {
 
@@ -35,16 +39,99 @@ public class SortedLinkedListPriorityQueue extends Queue {
 
     @Override
     public void enqueue(String element) {
+        OperationHistoryDto operationHistory = MemoryUtils.getLastMemorySnapshot("enqueue", memoryHistory, "element", element);
+        operationHistory.addLocalVariable("element", element);
 
+        String headAddress = (String) operationHistory.getInstanceVariableValue("head");
+        Node head = headAddress != null ? convertToNode(operationHistory.getObject(headAddress)) : null;
+
+        if (head == null || element.compareTo(head.getValue()) < 0) {
+            Node newNode = new Node();
+            newNode.setValue(element);
+            newNode.setNextAddress(headAddress);
+
+            String addrs = operationHistory.addNewObject(newNode);
+            operationHistory.addInstanceVariable("head", addrs);
+        }else {
+            String current = headAddress;
+            operationHistory.addLocalVariable("current", current);
+
+            Node currentNode = convertToNode(operationHistory.getObject(current));
+
+            String prev = null;
+            operationHistory.addLocalVariable("prev", prev);
+
+            while (current != null && currentNode.getValue().compareTo(element) <= 0) {
+                prev = current;
+                current = currentNode.getNextAddress();
+                currentNode = current != null ? convertToNode(operationHistory.getObject(current)) : null;
+            }
+
+            Node newPrev = new Node();
+            newPrev.setValue(element);
+            newPrev.setNextAddress(current);
+
+            String address = operationHistory.addNewObject(newPrev);
+            Node prevNode = convertToNode(operationHistory.getObject(prev));
+            prevNode.setNextAddress(address);
+
+            operationHistory.updateObject(prev, prevNode);
+
+            operationHistory.removeLocalVariable("prev");
+            operationHistory.removeLocalVariable("current");
+        }
+
+        int count = getCount(operationHistory);
+        count++;
+        operationHistory.addInstanceVariable("count", count);
+
+        memoryHistory.addOperationHistory(operationHistory);
     }
 
     @Override
     public void dequeue() {
+        OperationHistoryDto operationHistory = MemoryUtils.getLastMemorySnapshot("dequeue", memoryHistory);
 
+        int count = getCount(operationHistory);
+
+        if (count == 0) {
+            throw new IllegalArgumentException("Attempting to dequeue an empty queue");
+        }
+
+        String headAddress = (String) operationHistory.getInstanceVariableValue("head");
+        Node headNode = convertToNode(operationHistory.getObject(headAddress));
+
+        String result = headNode.getValue();
+        operationHistory.addLocalVariable("result", result);
+
+        String oldNode = headAddress;
+        operationHistory.addLocalVariable("oldNode", oldNode);
+
+        operationHistory.addInstanceVariable("head", headNode.getNextAddress());
+        operationHistory.freeLocalVariable("oldHead", "oldHead freed");
+
+        count--;
+        operationHistory.addInstanceVariable("count", count);
+
+        operationHistory.addResult(result);
+        operationHistory.removeLocalVariable("result");
+
+        memoryHistory.addOperationHistory(operationHistory);
     }
 
     @Override
     public void peek() {
+        OperationHistoryDto operationHistory = MemoryUtils.getLastMemorySnapshot("peek", memoryHistory);
 
+        int count = getCount(operationHistory);
+
+        if (count == 0) {
+            throw new IllegalArgumentException("Attempting to peek an empty queue");
+        }
+
+        Node headNode = convertToNode(operationHistory.getObject((String) operationHistory.getInstanceVariableValue("head")));
+        operationHistory.addResult(headNode.getValue());
+
+        memoryHistory.addOperationHistory(operationHistory);
     }
 }
--- src/main/java/org/gpavl/datastructuresvisualizationbackend/model/queue/UnsortedDoublyLinkedListPriorityQueue.java
+++ src/main/java/org/gpavl/datastructuresvisualizationbackend/model/queue/UnsortedDoublyLinkedListPriorityQueue.java
@@ -1,15 +1,36 @@
 package org.gpavl.datastructuresvisualizationbackend.model.queue;
 
+import org.gpavl.datastructuresvisualizationbackend.model.DoublyLinkedNode;
+import org.gpavl.datastructuresvisualizationbackend.model.MemoryHistoryDto;
+import org.gpavl.datastructuresvisualizationbackend.model.OperationHistoryDto;
+import org.gpavl.datastructuresvisualizationbackend.util.MemoryUtils;
+
+import java.util.Collections;
+
+import static org.gpavl.datastructuresvisualizationbackend.util.MemoryUtils.convertToDoublyLinkedNode;
+import static org.gpavl.datastructuresvisualizationbackend.util.MemoryUtils.getCount;
+
 public class UnsortedDoublyLinkedListPriorityQueue extends Queue {
+
+    public UnsortedDoublyLinkedListPriorityQueue() {
+        memoryHistory = new MemoryHistoryDto();
+        OperationHistoryDto operationHistory = new OperationHistoryDto("UnsortedDoublyLinkedListPriorityQueue", Collections.emptyMap());
+
+        operationHistory.addInstanceVariable("head", null);
+        operationHistory.addInstanceVariable("tail", null);
+        operationHistory.addInstanceVariable("count", 0);
+
+        memoryHistory.addOperationHistory(operationHistory);
+    }
 
     @Override
     public void size() {
-
+        MemoryUtils.size(memoryHistory);
     }
 
     @Override
     public void isEmpty() {
-
+        MemoryUtils.isEmpty(memoryHistory);
     }
 
     @Override
@@ -19,16 +40,136 @@ public class UnsortedDoublyLinkedListPriorityQueue extends Queue {
 
     @Override
     public void enqueue(String element) {
+        OperationHistoryDto operationHistory = MemoryUtils.getLastMemorySnapshot("enqueue", memoryHistory, "element", element);
+        operationHistory.addLocalVariable("element", element);
 
+        String headAddress = (String) operationHistory.getInstanceVariableValue("head");
+
+        DoublyLinkedNode doublyLinkedNode = new DoublyLinkedNode();
+        doublyLinkedNode.setValue(element);
+        doublyLinkedNode.setNextAddress(headAddress);
+
+        String newNodeAddress = operationHistory.addNewObject(doublyLinkedNode);
+        operationHistory.addLocalVariable("newNode", newNodeAddress);
+
+        if (headAddress != null) {
+            DoublyLinkedNode headNode = convertToDoublyLinkedNode(operationHistory.getObject(headAddress));
+            headNode.setPreviousAddress(newNodeAddress);
+
+            operationHistory.updateObject(headAddress, headNode);
+        }else {
+            operationHistory.addInstanceVariable("tail", newNodeAddress);
+        }
+
+        operationHistory.addInstanceVariable("head", newNodeAddress);
+
+        int count = getCount(operationHistory);
+        count++;
+        operationHistory.addInstanceVariable("count", count);
+
+        operationHistory.removeLocalVariable("newNode");
+        operationHistory.removeLocalVariable("element");
+
+        memoryHistory.addOperationHistory(operationHistory);
     }
 
     @Override
     public void dequeue() {
+        OperationHistoryDto operationHistory = MemoryUtils.getLastMemorySnapshot("dequeue", memoryHistory);
 
+        int count = getCount(operationHistory);
+
+        if (count == 0) {
+            throw new IllegalArgumentException("Attempting to dequeue an empty queue");
+        }
+
+        String minNode = (String) operationHistory.getInstanceVariableValue("head");
+        DoublyLinkedNode minn = convertToDoublyLinkedNode(operationHistory.getObject(minNode));
+        operationHistory.addLocalVariable("minNode", minNode);
+
+        String current = minn.getNextAddress();
+        DoublyLinkedNode currentNode = convertToDoublyLinkedNode(operationHistory.getObject(current));
+        operationHistory.addLocalVariable("current", current);
+
+        while (current != null) {
+            if (currentNode.getValue().compareTo(minn.getValue()) < 0) {
+                minNode = current;
+                operationHistory.addLocalVariable("minNode", minNode);
+                minn = convertToDoublyLinkedNode(operationHistory.getObject(minNode));
+            }
+            current = currentNode.getNextAddress();
+            operationHistory.addLocalVariable("current", current);
+            currentNode = current != null ? convertToDoublyLinkedNode(operationHistory.getObject(current)) : null;
+        }
+
+        String result = minn.getValue();
+        operationHistory.addLocalVariable("result", result);
+
+        if (minn.getPreviousAddress() != null) {
+            DoublyLinkedNode prv = convertToDoublyLinkedNode(operationHistory.getObject(minn.getPreviousAddress()));
+            prv.setNextAddress(minn.getNextAddress());
+
+            operationHistory.updateObject(minn.getPreviousAddress(), prv);
+        }else {
+            operationHistory.addInstanceVariable("head", minn.getNextAddress());
+        }
+
+        if (minn.getNextAddress() != null) {
+            DoublyLinkedNode prv = convertToDoublyLinkedNode(operationHistory.getObject(minn.getNextAddress()));
+            prv.setPreviousAddress(minn.getPreviousAddress());
+
+            operationHistory.updateObject(minn.getNextAddress(), prv);
+        }else {
+            operationHistory.addInstanceVariable("tail", minn.getPreviousAddress());
+        }
+
+        operationHistory.freeLocalVariable("minNode", "minNode freed");
+
+        count--;
+        operationHistory.addInstanceVariable("count", count);
+
+        operationHistory.addResult(result);
+
+        operationHistory.removeLocalVariable("result");
+        operationHistory.removeLocalVariable("current");
+
+        memoryHistory.addOperationHistory(operationHistory);
     }
 
     @Override
     public void peek() {
+        OperationHistoryDto operationHistory = MemoryUtils.getLastMemorySnapshot("peek", memoryHistory);
 
+        int count = getCount(operationHistory);
+
+        if (count == 0) {
+            throw new IllegalArgumentException("Attempting to peek an empty queue");
+        }
+
+        String current = (String) operationHistory.getInstanceVariableValue("head");
+        operationHistory.addLocalVariable("current", current);
+
+        DoublyLinkedNode currentNode = convertToDoublyLinkedNode(operationHistory.getObject(current));
+        String minValue = currentNode.getValue();
+        operationHistory.addLocalVariable("minValue", minValue);
+
+        current = currentNode.getNextAddress();
+        operationHistory.addLocalVariable("current", current);
+
+        while (current != null) {
+            if (currentNode.getValue().compareTo(minValue) < 0) {
+                minValue = currentNode.getValue();
+                operationHistory.addLocalVariable("minValue", minValue);
+            }
+            current = currentNode.getNextAddress();
+            operationHistory.addLocalVariable("current", current);
+            currentNode = current != null ? convertToDoublyLinkedNode(operationHistory.getObject(current)) : null;
+        }
+
+        operationHistory.addResult(minValue);
+        operationHistory.removeLocalVariable("minValue");
+        operationHistory.removeLocalVariable("current");
+
+        memoryHistory.addOperationHistory(operationHistory);
     }
 }
